3ba8269adea81dc17773db37af0f3475da4f7d99
Yarik Henza <gsmeter@gmail.com>
2019-11-07 10:52:58 +0200

    rm extra backend

--- lab1/api/events.api.js
+++ lab1/api/events.api.js
@@ -4,7 +4,7 @@ const { backendUrl } = config;
 
 export default getList = async () => {
   const headers = { "content-type": "application/json" };
-  const url = `${backendUrl}/api/feed`;
+  const url = `${backendUrl}/api/events`;
 
   const response = await fetch(url, { headers });
   const json = await response.json();
